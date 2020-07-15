713a2870af5b143b7af136ef3d04e9083707e9f2
Vedant Puri <vpuri3@illinois.edu>
2020-07-15 10:12:30 -0500

    compare nalu, quic, nek, snyder

--- mtlb/patrick/compare.m
+++ mtlb/patrick/compare.m
@@ -25,7 +25,7 @@
 
 %=======================================================
 function compare(al)
-%al=45;
+%al=90;
 
 uscl=0.5;
 kscl=2.0;
@@ -51,7 +51,7 @@ tk=dlmread([dir,'var.dat'],'' ,[1 1 n 3]); % < u' * u' >
 xN=C(:,1);
 yN=C(:,2);
 zN=C(:,3);
-[xN,yN,zN] = insidecube(al,xN,yN,zN);
+%[xN,yN,zN] = insidecube(al,xN,yN,zN);
 
 uN=U(:,1);
 vN=U(:,2);
@@ -66,8 +66,7 @@ kN = 0.5 * sum(tk')';
 
 I1=         1:nx1*ny1*nz1 ; I1=reshape(I1,[nx1,ny1,nz1]);
 I2=I1(end)+(1:nx2*ny2*nz2); I2=reshape(I2,[nx2,ny2,nz2]);
-I1ref0=I1(1,0.5*ny1  );
-I1ref1=I1(1,0.5*ny1+1);
+I1ref=I1(1,334);
 I2ref=I2(1);
 
 %=======================================================
@@ -118,7 +117,7 @@ elseif(al==90)
 	vvE1=M(:,6)*ufactor; % w -> v
 	kE1 = 0.75*(uuE1.*uuE1+vvE1.*vvE1);
 end
-[xE1,yE1,zE1] = insidecube(al,xE1,yE1,zE1);
+%[xE1,yE1,zE1] = insidecube(al,xE1,yE1,zE1);
 
 % Reshaping EPA
 if(al==45)
@@ -154,60 +153,100 @@ uN1=M(:,2:5); uN1=uN1/uN1(4,1);
 M=readmatrix(f2);
 xN2=M(:,1);
 uN2=M(:,2); uN2=uN2/uN2(1);
+%=======================================================
+% QUIC
+%=======================================================
+load('./QUICprofs/U_3H_45dg.mat');
+load('./QUICprofs/U_3H_90dg.mat');
+load('./QUICprofs/U_6H_45dg.mat');
+load('./QUICprofs/U_6H_90dg.mat');
+load('./QUICprofs/U_8H_45dg.mat');
+load('./QUICprofs/U_8H_90dg.mat');
+load('./QUICprofs/U_CL_1_5H_45dg.mat');
+load('./QUICprofs/U_CL_1_6H_90dg.mat');
+load('./QUICprofs/U_inflow_45dg.mat');
+load('./QUICprofs/U_inflow_90dg.mat');
+load('./QUICprofs/x_QUIC.mat');
+load('./QUICprofs/z_QUIC.mat');
+
+xQ=x_QUIC;
+yQ=z_QUIC;
+if(al==45)
+	uQ1=[U_inflow_45dg;U_3H_45dg;U_6H_45dg;U_8H_45dg];
+	uQ2=U_CL_1_5H_45dg;
+elseif(al==90)
+	uQ1=[U_inflow_90dg;U_3H_90dg;U_6H_90dg;U_8H_90dg];
+	uQ2=U_CL_1_6H_90dg;
+end
+xxQ=[-4,3,6,8];
+
+uQ1=uQ1 / (0.5*(uQ1(1,21)+uQ1(1,21)));
+uQ2=uQ2 / (0.5*(uQ2(110) +uQ2(111) ));
 
 %=======================================================
-% plots
-%=======================================================
 % vertical profiles
-uE1=uE1 / uE1(1,end-2);
-uNref = 0.5*(uN(I1ref0) + uN(I1ref1));
-uN =uN  / uNref;
-figure;fig=gcf;ax=gca; hold on;grid on;
-ax.XScale='linear';
-ax.YScale='linear';
-xlabel(['$$v_x$$']);
-ylabel('$$y$$');
-xlim([0,1.2]);
-ylim([0,2.0]);
+%=======================================================
+% normalization
+if(al==45)     uE1=uE1 / uE1(1,end-5);
+elseif(al==90) uE1=uE1 / uE1(1,end-6);
+end
+uN =uN  / uN(I1ref);
+
+% figure
 nS=size(xE1,1);
 SS=[1,nS-3,nS-1,nS];
-cc='rgbk';
+
 for i=1:nx1
-	II =I1(i,:);
-	II0=II(1);
-	p=plot(uN(II),yN(II),['-',cc(i)],'linewidth',2);
+	figure;fig=gcf;ax=gca; hold on;grid on;
+	ax.XScale='linear';  ax.YScale='linear';
+	xlabel(['$$v_x$$']); ylabel('$$y$$');
+	xlim([0,1.2]);       ylim([0,2.0]);
+	cc='rgbk';
+
+	II =I1(i,:); II0=II(1);
+	p=plot(uN(II),yN(II),['-',cc(1)],'linewidth',2);
 	p.DisplayName=['Nek x=',num2str(xN(II0))];
 	
 	nx=SS(i);
-	p=plot(uE1(nx,:),yE1(nx,:),['-o',cc(i)],'linewidth',2);
+	p=plot(uE1(nx,:),yE1(nx,:),['-o',cc(2)],'linewidth',2);
 	p.DisplayName=['Sny x=',num2str(xN(II0))];
 
-	p=plot(uN1(:,i),yN1,['-.',cc(i)],'linewidth',2);
-	p.DisplayName=['Nalu x=',num2str(xN1(i))];
-end
-title(['WMC',num2str(al),' x-velocity']);
-legend('show','location','northwest');
-%
-figname=['WMC',num2str(al),'vertical profile x-velocity'];
-saveas(fig,figname,'png');
+	p=plot(uQ1(i,:),yQ,['-',cc(3)],'linewidth',2);
+	p.DisplayName=['QUIC x=',num2str(xxQ(i))];
 
+	p=plot(uN1(:,i),yN1,['-',cc(4)],'linewidth',2);
+	p.DisplayName=['Nalu x=',num2str(xN1(i))];
+
+	title(['WMC',num2str(al),' x-velocity at x=',num2str(xxQ(i))]);
+	legend('show','location','northwest');
+	figname=['WMC',num2str(al),'x',num2str(i)];
+	saveas(fig,figname,'png');
+end
+
+%=======================================================
 % streamwise transect
+%=======================================================
+% normalization
 uE1=uE1 / uE1(1,end-2);
 uN =uN  / uN (I2ref);
 
+% figure
 figure;fig=gcf;ax=gca; hold on;grid on;
 ax.XScale='linear';
 ax.YScale='linear';
 xlabel(['$$x$$']);
 ylabel('$$v_x$$');
 
-p=plot(xN(I2),uN(I2),'b-','linewidth',2.0);
+p=plot(xN(I2),uN(I2),['-',cc(1)],'linewidth',2.0);
 p.DisplayName=['Nek, y=',num2str(yN(I2ref))];
 
-p=plot(xE1(:,end-2),uE1(:,end-2),'ro','linewidth',2);
+p=plot(xE1(:,end-2),uE1(:,end-2),['o',cc(2)],'linewidth',2);
 p.DisplayName=['Sny, y=',num2str(yE1(1,end-2))];
 
-p=plot(xN2,uN2,'g-','linewidth',2);
+p=plot(xQ,uQ2,['-',cc(3)],'linewidth',2);
+p.DisplayName=['QUIC'];
+
+p=plot(xN2,uN2,['-',cc(4)],'linewidth',2);
 p.DisplayName=['Nalu'];
 
 p=plot(xw,yw*0.02+1,'k-.','linewidth',2);
